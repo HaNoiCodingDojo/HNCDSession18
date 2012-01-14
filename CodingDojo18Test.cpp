10238fd4c89899e9a71a63dda763da403f76abb2
Benoit Bayol <benoit.bayol@gmail.com>
2012-01-14 14:40:52 +0700

    commit

--- CodingDojo18Test.cpp
+++ CodingDojo18Test.cpp
@@ -1,15 +1,27 @@
 #define BOOST_TEST_MODULE CodingDojo18
 #include <boost/test/included/unit_test.hpp>
 
-int function()
+#include <string>
+#include <vector>
+
+std::string evaluate(std::vector<int>)
 {
+    return "";
 }
 
 BOOST_AUTO_TEST_SUITE(BOOST_TEST_MODULE)
 
-BOOST_AUTO_TEST_CASE(test1)
+BOOST_AUTO_TEST_CASE(evaluate_5_5_5_5_5_return_FIVE)
 {
+    std::vector<int> hand(5);
+    hand[0] = 5;
+    hand[1] = 5;
+    hand[2] = 5;
+    hand[3] = 5;
+    hand[4] = 5;
+    BOOST_CHECK_EQUAL("FIVE", evaluate(hand));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
 #undef BOOST_TEST_MODULE
+
