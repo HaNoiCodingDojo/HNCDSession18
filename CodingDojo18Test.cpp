70f17c40c3ee82d0b22d8de6b239bb05f422375c
Benoit Bayol <benoit.bayol@gmail.com>
2012-01-14 15:16:34 +0700

    commit

--- CodingDojo18Test.cpp
+++ CodingDojo18Test.cpp
@@ -25,22 +25,21 @@ BOOST_AUTO_TEST_SUITE(BOOST_TEST_MODULE)
 BOOST_AUTO_TEST_CASE(evaluate_5_5_5_5_5_return_FIVE)
 {
     vector<int> hand(5);
-    hand[0] = 5;
-    hand[1] = 5;
-    hand[2] = 5;
-    hand[3] = 5;
-    hand[4] = 5;
+    hand[0] = 5; hand[1] = 5; hand[2] = 5; hand[3] = 5; hand[4] = 5;
     BOOST_CHECK_EQUAL("FIVE", evaluate(hand));
 }
 
 BOOST_AUTO_TEST_CASE(evaluate_1_1_5_5_5_return_FULL)
 {
     vector<int> hand(5);
-    hand[0] = 1;
-    hand[1] = 1;
-    hand[2] = 5;
-    hand[3] = 5;
-    hand[4] = 5;
+    hand[0] = 1; hand[1] = 1; hand[2] = 5; hand[3] = 5; hand[4] = 5;
+    BOOST_CHECK_EQUAL("FULL", evaluate(hand));
+}
+
+BOOST_AUTO_TEST_CASE(evaluate_1_1_1_5_5_return_FULL)
+{
+    vector<int> hand(5);
+    hand[0] = 1; hand[1] = 1; hand[2] = 1; hand[3] = 5; hand[4] = 5;
     BOOST_CHECK_EQUAL("FULL", evaluate(hand));
 }
 
@@ -76,6 +75,7 @@ BOOST_AUTO_TEST_CASE(evaluate_1_1_1_1_5_return_FOUR)
     hand[4] = 5;
     BOOST_CHECK_EQUAL("FOUR", evaluate(hand));
 }
+
 
 BOOST_AUTO_TEST_SUITE_END()
 #undef BOOST_TEST_MODULE
