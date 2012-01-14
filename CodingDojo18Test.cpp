0dc373aea75475eb8516ffdbc4d31f5db6c26a06
Benoit Bayol <benoit.bayol@gmail.com>
2012-01-14 15:09:55 +0700

    commit

--- CodingDojo18Test.cpp
+++ CodingDojo18Test.cpp
@@ -63,6 +63,17 @@ BOOST_AUTO_TEST_CASE(evaluate_5_1_5_5_5_return_FOUR)
     BOOST_CHECK_EQUAL("FOUR", evaluate(hand));
 }
 
+BOOST_AUTO_TEST_CASE(evaluate_1_1_1_1_5_return_FOUR)
+{
+    vector<int> hand(5);
+    hand[0] = 1;
+    hand[1] = 1;
+    hand[2] = 1;
+    hand[3] = 1;
+    hand[4] = 5;
+    BOOST_CHECK_EQUAL("FOUR", evaluate(hand));
+}
+
 BOOST_AUTO_TEST_SUITE_END()
 #undef BOOST_TEST_MODULE
 
